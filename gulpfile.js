4e32c29a0960fcbca5c6096039d04e0b9036cd41
Oleksii Grygorov <leha.grig@gmail.com>
2018-10-20 15:21:50 +0300

    subheader - finished

--- gulpfile.js
+++ gulpfile.js
@@ -72,9 +72,9 @@ gulp.task('serve', function (){
     gulp.watch('./src/scss/**/*.scss',['sass']).on('change', browserSync.reload);
     gulp.watch('./src/js/*.js',['minify']).on('change', browserSync.reload);
     gulp.watch('./src/index.html').on('change', function(){
-        return gulp.src('./src/index.html').pipe(gulp.dest('./build'))
+        return gulp.src('./src/index.html').pipe(gulp.dest('./build'));
     });
-    gulp.watch('./build/index.html').on('change', browserSync.reload);
+    gulp.watch('./src/index.html').on('change', browserSync.reload);
 
 })
 
